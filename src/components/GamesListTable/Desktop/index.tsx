2dfae4926107d843eb839855fb95aa2dad3bb463
tienviet10 <viettran101294@gmail.com>
2023-09-01 02:26:22 -0400

    removed unused codes

--- src/components/GamesListTable/Desktop/index.tsx
+++ src/components/GamesListTable/Desktop/index.tsx
@@ -20,7 +20,6 @@ function UserGameListDesktop({ data }: UserGameListDataType) {
   const { userGameDataIsLoading } = useGetUserGameState(chosenGame?.id);
 
   const handleClick = async (game: GameDataType) => {
-    console.log('game', game);
     setChosenGame(game);
     setOpen(true);
   };
